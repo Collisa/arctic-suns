6af3008723357f57f743aa8c541baa23a71d4ca1
LisaCollignon <68648800+LisaCollignon@users.noreply.github.com>
2020-08-21 10:39:15 +0200

    Created Structure in app

---
+++ app/__init__.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request, jsonify, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import Form, StringField, SubmitField, TextAreaField
+from wtforms.fields.html5 import DateField
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from os import environ
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+
+
+app.config['SECRET_KEY'] = 'c5723c7a0956175ca1e56086adc6bdc18064d2659be3b04fcd3bb89d1fbfe482'
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL') or 'sqlite:///arctic_DB2.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+ 
+
+
+class ArcticSun(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(25))
+    date_in = db.Column(db.Date)
+    date_out = db.Column(db.Date)
+    pick_up_location = db.Column(db.String(100))
+    destination = db.Column(db.String(100))
+    status = db.Column(db.String(15))
+    remarks = db.Column(db.String(100))
+
+    # def __init__(self, id, name, date_in, date_out, pick_up_location, destination, status):
+    #     self.id = id
+    #     self.name = name
+    #     self.date_in = date_in
+    #     self.date_out = date_out
+    #     self.pick_up_location = pick_up_location
+    #     self.destination = destination
+    #     self.status = status    
+
+#db.create_all()
+
+
+class Input(FlaskForm):
+    name = StringField("Arctic sun SN")
+    date_in = DateField("Afhaling")
+    date_out = DateField("Levering")
+    pick_up_location = TextAreaField("Afhaallocatie")
+    destination = TextAreaField("Leverlocatie")
+    status = StringField("Status")
+    submit = SubmitField("Toevoegen")
+    remarks = TextAreaField("Opmerkingen")
+
+
