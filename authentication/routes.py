7507efc1e7612fc0b05c1cb5c65ea500c25f3644
Lisa Collignon <68648800+LisaCollignon@users.noreply.github.com>
2020-10-16 20:20:37 +0200

    register url blocked for Bard accounts

--- authentication/routes.py
+++ authentication/routes.py
@@ -9,15 +9,16 @@ from flask_login import login_user, current_user, logout_user, login_required
 @app.route("/register", methods=["GET", "POST"])
 @login_required
 def register():
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        hashed_pw = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
-        user = User(username=form.username.data, email=form.email.data, password=hashed_pw, firm=form.firm.data)
-        db.session.add(user)
-        db.session.commit()
-        flash("Your account has been created! You are now able to log in.", "succes")
-        return redirect(url_for("arcticsun_index", device_type="arctic"))
-    return render_template("authentication/register.html", title="Register", form=form)
+    if current_user.firm == "Collibri":
+        form = RegistrationForm()
+        if form.validate_on_submit():
+            hashed_pw = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+            user = User(username=form.username.data, email=form.email.data, password=hashed_pw, firm=form.firm.data)
+            db.session.add(user)
+            db.session.commit()
+            flash("Your account has been created! You are now able to log in.", "succes")
+            return redirect(url_for("arcticsun_index", device_type="arctic"))
+        return render_template("authentication/register.html", title="Register", form=form)
 
 @app.route("/", methods=["GET", "POST"])
 @app.route("/login", methods=["GET", "POST"])
