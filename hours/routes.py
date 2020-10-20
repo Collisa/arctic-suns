219701757ba723da57181e9b123aeb409c8fb656
Lisa Collignon <68648800+LisaCollignon@users.noreply.github.com>
2020-10-20 19:54:06 +0200

    added current_user to every route to limit bard

--- hours/routes.py
+++ hours/routes.py
@@ -13,7 +13,6 @@ from hours.weekly_view import get_weekly_view_days
 import locale
 import copy
 from dateutil.relativedelta import relativedelta
-
 weekDays = ("Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag", "Zondag")
 
 
@@ -21,110 +20,114 @@ weekDays = ("Maandag", "Dinsdag", "Woensdag", "Donderdag", "Vrijdag", "Zaterdag"
 @app.route('/hours', methods=["GET"])
 @login_required
 def hours_index():
-    days_off = 21
-    
-    year, month = calculate_month_view()
-    locale.setlocale(locale.LC_ALL, 'nl_NL')
-    # month_view = calendar.month(year, month)
-    month_view = calendar.TextCalendar().formatmonth(year, month)
+    if current_user.firm == 'Collibri':
+        days_off = 21
+        
+        year, month = calculate_month_view()
+        locale.setlocale(locale.LC_ALL, 'nl_NL')
+        # month_view = calendar.month(year, month)
+        month_view = calendar.TextCalendar().formatmonth(year, month)
 
-    person_id = request.args.get('person_id', '1')
+        person_id = request.args.get('person_id', '1')
 
-    extrahours_year, leavedays = calculate_extrahours_year(person_id)
-    
-    data = {
-        'extrahours_week': calculate_extrahours_week(person_id),
-        'extrahours_month': calculate_extrahours_month(person_id),
-        'extrahours_year': extrahours_year,
-        'leavedays': leavedays,
-        'weeklyview_days': get_weekly_view_days(person_id),
-        'month': month
-    }
+        extrahours_year, leavedays = calculate_extrahours_year(person_id)
+        
+        data = {
+            'extrahours_week': calculate_extrahours_week(person_id),
+            'extrahours_month': calculate_extrahours_month(person_id),
+            'extrahours_year': extrahours_year,
+            'leavedays': leavedays,
+            'weeklyview_days': get_weekly_view_days(person_id),
+            'month': month
+        }
 
-    return render_template("hours/index.html", days_off=days_off, template_form=HoursForm(), person_form=PersonForm(person_id=person_id), person_id=person_id, data=data, month_view=month_view, current_user=current_user)
+        return render_template("hours/index.html", days_off=days_off, template_form=HoursForm(), person_form=PersonForm(person_id=person_id), person_id=person_id, data=data, month_view=month_view, current_user=current_user)
 
 
 
 @app.route('/hours/add', methods=["POST"])
 @login_required
 def hours_add():
-    hour_form = HoursForm(request.form)
-    
-    extra_hours = 0
+    if current_user.firm == 'Collibri':
+        hour_form = HoursForm(request.form)
+        
+        extra_hours = 0
 
-    if hour_form.end_hour.data:
-        extra_hours = datetime.combine(date.today(), hour_form.end_hour.data) - datetime.combine(date.today(), hour_form.start_hour.data)
-        extra_hours = extra_hours - timedelta(hours=8)
-        extra_hours = int(extra_hours.total_seconds() / 60)
+        if hour_form.end_hour.data:
+            extra_hours = datetime.combine(date.today(), hour_form.end_hour.data) - datetime.combine(date.today(), hour_form.start_hour.data)
+            extra_hours = extra_hours - timedelta(hours=8)
+            extra_hours = int(extra_hours.total_seconds() / 60)
 
-    try:
-        day = Employee(
-            person_id=hour_form.person_id.data,
-            workday=hour_form.workday.data,
-            start_hour=hour_form.start_hour.data,
-            end_hour=hour_form.end_hour.data,
-            type_day=hour_form.type_day.data,
-            total_leave_days= 1 if hour_form.type_day.data == "verlof" else 0,
-            extra_hours=extra_hours)
-        db.session.add(day)
-        db.session.commit()
-    
-    except IntegrityError:
-        return redirect(url_for('hours_edit', datum=day.workday, id=day.person_id, type_day=day.type_day, start_hour=day.start_hour, end_hour=day.end_hour))
+        try:
+            day = Employee(
+                person_id=hour_form.person_id.data,
+                workday=hour_form.workday.data,
+                start_hour=hour_form.start_hour.data,
+                end_hour=hour_form.end_hour.data,
+                type_day=hour_form.type_day.data,
+                total_leave_days= 1 if hour_form.type_day.data == "verlof" else 0,
+                extra_hours=extra_hours)
+            db.session.add(day)
+            db.session.commit()
+        
+        except IntegrityError:
+            return redirect(url_for('hours_edit', datum=day.workday, id=day.person_id, type_day=day.type_day, start_hour=day.start_hour, end_hour=day.end_hour))
 
-    return redirect(url_for('hours_index', person_id=day.person_id))
+        return redirect(url_for('hours_index', person_id=day.person_id))
 
 @app.route('/hours/edit/<int:id>/<datum>/', methods=["GET", "POST"])
 @login_required
 def hours_edit(id, datum):
-    item_to_change = Employee.query.filter(Employee.person_id == id, Employee.workday == datum).first()
+    if current_user.firm == 'Collibri':
+        item_to_change = Employee.query.filter(Employee.person_id == id, Employee.workday == datum).first()
 
-    extra_hours = 0
+        extra_hours = 0
 
-    if request.method == "POST":
-        form = HoursForm(request.form)
-        
-        if form.end_hour.data:
+        if request.method == "POST":
+            form = HoursForm(request.form)
             
-            extra_hours = datetime.combine(date.today(), form.end_hour.data) - datetime.combine(date.today(), form.start_hour.data)
-            extra_hours = extra_hours - timedelta(hours=8)
-            extra_hours = int(extra_hours.total_seconds() / 60) 
+            if form.end_hour.data:
+                
+                extra_hours = datetime.combine(date.today(), form.end_hour.data) - datetime.combine(date.today(), form.start_hour.data)
+                extra_hours = extra_hours - timedelta(hours=8)
+                extra_hours = int(extra_hours.total_seconds() / 60) 
 
-        item_to_change.person_id=id
-        item_to_change.workday=form.workday.data
-        item_to_change.start_hour=form.start_hour.data
-        item_to_change.end_hour=form.end_hour.data
-        item_to_change.type_day=form.type_day.data
-        item_to_change.total_leave_days=1 if form.type_day.data == "verlof" else 0
-        item_to_change.extra_hours=extra_hours
+            item_to_change.person_id=id
+            item_to_change.workday=form.workday.data
+            item_to_change.start_hour=form.start_hour.data
+            item_to_change.end_hour=form.end_hour.data
+            item_to_change.type_day=form.type_day.data
+            item_to_change.total_leave_days=1 if form.type_day.data == "verlof" else 0
+            item_to_change.extra_hours=extra_hours
 
-        db.session.commit()
+            db.session.commit()
 
-        return redirect(url_for('hours_index', person_id=id))
+            return redirect(url_for('hours_index', person_id=id))
 
 
-    item_to_change2 = copy.deepcopy(item_to_change)
-    if(request.args.get('start_hour')):
-        item_to_change2.start_hour=datetime.strptime(request.args.get('start_hour'), '%H:%M:%S')
+        item_to_change2 = copy.deepcopy(item_to_change)
+        if(request.args.get('start_hour')):
+            item_to_change2.start_hour=datetime.strptime(request.args.get('start_hour'), '%H:%M:%S')
 
-    if(request.args.get('end_hour')):
-        item_to_change2.end_hour=datetime.strptime(request.args.get('end_hour'), '%H:%M:%S')
-    
-    item_to_change2.type_day=request.args.get('type_day')
+        if(request.args.get('end_hour')):
+            item_to_change2.end_hour=datetime.strptime(request.args.get('end_hour'), '%H:%M:%S')
+        
+        item_to_change2.type_day=request.args.get('type_day')
 
-    return render_template('hours/edit.html', template_form=HoursForm(obj=item_to_change2), item_to_change=item_to_change, id=id, datum=datum, current_user=current_user)
+        return render_template('hours/edit.html', template_form=HoursForm(obj=item_to_change2), item_to_change=item_to_change, id=id, datum=datum, current_user=current_user)
 
 
 @app.route('/hours/month/<int:id>/<int:month>')
 @login_required
 def month_view(id, month):
-    firstdayofmonth = datetime.today().replace(month=month, day=1) - timedelta(days=1)
+    if current_user.firm == 'Collibri':
+        firstdayofmonth = datetime.today().replace(month=month, day=1) - timedelta(days=1)
 
-    lastdayofmonth = (firstdayofmonth.replace(day=1) + relativedelta(months=+2) - timedelta(days=1))
+        lastdayofmonth = (firstdayofmonth.replace(day=1) + relativedelta(months=+2) - timedelta(days=1))
 
-    qry = Employee.query.filter(Employee.person_id == id,
-                                Employee.workday.between(firstdayofmonth, lastdayofmonth)).order_by(Employee.workday.asc())
+        qry = Employee.query.filter(Employee.person_id == id,
+                                    Employee.workday.between(firstdayofmonth, lastdayofmonth)).order_by(Employee.workday.asc())
 
-    return render_template('hours/month-view.html', id=id, qry=qry, weekDays=weekDays, current_user=current_user)
+        return render_template('hours/month-view.html', id=id, qry=qry, weekDays=weekDays, current_user=current_user)
 
 
--- kaarten/routes.py
+++ kaarten/routes.py
@@ -8,52 +8,57 @@ from authentication.models import User
 @app.route('/kaarten', methods=["GET"])
 @login_required
 def kaart_index():
-    all_cards = Card.query.filter(Card.date_retour == None)
-    return render_template("kaarten/index.html", template_form=CardForm(), all_cards=all_cards, current_user=current_user)
+    if current_user.firm == 'Collibri':
+        all_cards = Card.query.filter(Card.date_retour == None)
+        return render_template("kaarten/index.html", template_form=CardForm(), all_cards=all_cards, current_user=current_user)
 
 
 @app.route('/kaarten/all', methods=["GET"])
 @login_required
 def kaart_show_all():
-    all_cards = Card.query.all()
-    return render_template("kaarten/index.html", template_form=CardForm(), all_cards=all_cards, current_user=current_user)
+    if current_user.firm == 'Collibri':
+        all_cards = Card.query.all()
+        return render_template("kaarten/index.html", template_form=CardForm(), all_cards=all_cards, current_user=current_user)
 
 
 @app.route('/kaart/add', methods=["POST"])
 @login_required
 def add_card():
-    form = CardForm(request.form)
-    if request.method == 'POST':
-        card = Card(
-            card=form.card.data, 
-            date_retour=form.date_retour.data,
-            date_out=form.date_out.data,
-            chauffeur=form.chauffeur.data)
-        db.session.add(card)
-        db.session.commit()
-    return redirect(url_for('kaart_index'))    
+    if current_user.firm == 'Collibri':
+        form = CardForm(request.form)
+        if request.method == 'POST':
+            card = Card(
+                card=form.card.data, 
+                date_retour=form.date_retour.data,
+                date_out=form.date_out.data,
+                chauffeur=form.chauffeur.data)
+            db.session.add(card)
+            db.session.commit()
+        return redirect(url_for('kaart_index'))    
 
 
 
 @app.route('/kaart/delete', methods=['POST'])
 @login_required
 def delete_card():
-    item_to_delete = Card.query.filter_by(id=request.form['id']).first()
-    db.session.delete(item_to_delete)
-    db.session.commit()
-    return ""
+    if current_user.firm == 'Collibri':
+        item_to_delete = Card.query.filter_by(id=request.form['id']).first()
+        db.session.delete(item_to_delete)
+        db.session.commit()
+        return ""
 
 @app.route('/kaart/change/<int:id>', methods=['GET','POST'])
 @login_required
 def change_card(id):
-    all_cards = Card.query.filter(Card.date_out == None)
-    item_to_change = Card.query.get(id)
-    form = CardForm(request.form)
-    if request.method == 'POST':
-        item_to_change.card=form.card.data
-        item_to_change.date_retour=form.date_retour.data
-        item_to_change.date_out=form.date_out.data
-        item_to_change.chauffeur=form.chauffeur.data
-        db.session.commit()
-        return redirect(url_for('kaart_index'))
-    return render_template("kaarten/index.html", template_form=CardForm(obj=item_to_change), all_cards=all_cards, edit=True, edit_id=item_to_change.id, current_user=current_user)
+    if current_user.firm == 'Collibri':
+        all_cards = Card.query.filter(Card.date_out == None)
+        item_to_change = Card.query.get(id)
+        form = CardForm(request.form)
+        if request.method == 'POST':
+            item_to_change.card=form.card.data
+            item_to_change.date_retour=form.date_retour.data
+            item_to_change.date_out=form.date_out.data
+            item_to_change.chauffeur=form.chauffeur.data
+            db.session.commit()
+            return redirect(url_for('kaart_index'))
+        return render_template("kaarten/index.html", template_form=CardForm(obj=item_to_change), all_cards=all_cards, edit=True, edit_id=item_to_change.id, current_user=current_user)
