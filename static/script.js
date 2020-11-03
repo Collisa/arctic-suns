7cdd78383c5ac6ef114091ac7b615139749fbc87
Lisa Collignon <68648800+LisaCollignon@users.noreply.github.com>
2020-11-03 20:21:56 +0100

    Added my info to the contact button

--- static/script.js
+++ static/script.js
@@ -1,1 +1,14 @@
- 
+const contactInfoButton = document.querySelector("#contact-info-button");
+const X = document.querySelector("#hide");
+const infoBlock = document.querySelector("#info-block");
+
+function showInfo() {
+    infoBlock.classList.remove("transform");
+}
+
+function hideInfo() {
+    infoBlock.classList.add("transform");
+}
+
+contactInfoButton.addEventListener("click", showInfo);
+hide.addEventListener("click", hideInfo);
