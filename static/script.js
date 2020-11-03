2b77e33f0310892019321b0b2dff36028e425d18
Lisa Collignon <68648800+LisaCollignon@users.noreply.github.com>
2020-11-03 20:49:15 +0100

    fixed footer not over wijzig button anymore

--- static/script.js
+++ static/script.js
@@ -3,11 +3,13 @@ const X = document.querySelector("#hide");
 const infoBlock = document.querySelector("#info-block");
 
 function showInfo() {
+    infoBlock.classList.remove("hidden");
     infoBlock.classList.remove("transform");
 }
 
 function hideInfo() {
     infoBlock.classList.add("transform");
+    infoBlock.classList.add("hidden");
 }
 
 contactInfoButton.addEventListener("click", showInfo);
