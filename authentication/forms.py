52338ea70ddf68774fd7e866a45a1e75915a75fc
Lisa Collignon <68648800+LisaCollignon@users.noreply.github.com>
2021-02-14 11:13:02 +0100

    Added self to decorators from RegistrationForm again, fixed registration problem with it

--- authentication/forms.py
+++ authentication/forms.py
@@ -25,14 +25,14 @@ class RegistrationForm(FlaskForm):
     )
     submit = SubmitField("Registreer")
 
-    def validate_username(username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError(
                 "That username is taken. Please choose a different one."
             )
 
-    def validate_email(email):
+    def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError("That email is taken. Please choose a different one.")
